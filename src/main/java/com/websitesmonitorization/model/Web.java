5ad370810bdc6d94a14fac8613843863e0e74732
LauraArtiles <laurartiles@gmail.com>
2021-10-23 14:06:14 +0200

    Crear clases de usuario y Web

---
+++ src/main/java/com/websitesmonitorization/model/Web.java
@@ -0,0 +1,43 @@
+package com.websitesmonitorization.model;
+
+public class Web {
+
+	String url;
+	String nombreWeb;
+	Integer idWeb;
+	String tipoWeb;
+	String update;
+	
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getNombreWeb() {
+		return nombreWeb;
+	}
+	public void setNombreWeb(String nombreWeb) {
+		this.nombreWeb = nombreWeb;
+	}
+	public Integer getIdWeb() {
+		return idWeb;
+	}
+	public void setIdWeb(Integer idWeb) {
+		this.idWeb = idWeb;
+	}
+	public String getTipoWeb() {
+		return tipoWeb;
+	}
+	public void setTipoWeb(String tipoWeb) {
+		this.tipoWeb = tipoWeb;
+	}
+	public String getUpdate() {
+		return update;
+	}
+	public void setUpdate(String update) {
+		this.update = update;
+	}
+	
+	
+}

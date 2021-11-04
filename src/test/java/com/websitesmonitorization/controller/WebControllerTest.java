c2db478e1b840a2a0dda99c2a4895b2c234824fd
Wing Kei <wingkei.liang.tan@alumnos.upm.es>
2021-11-04 20:06:55 +0100

    Add tests

---
+++ src/test/java/com/websitesmonitorization/controller/WebControllerTest.java
@@ -0,0 +1,14 @@
+package com.websitesmonitorization.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WebControllerTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}

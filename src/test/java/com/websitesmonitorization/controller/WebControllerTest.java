9441eae6d86890039974e3c3feef35d8c4db3a74
LauraArtiles <laurartiles@gmail.com>
2021-11-11 02:12:10 +0100

    [Test] Test de la clase WebController y UsuarioController

--- src/test/java/com/websitesmonitorization/controller/WebControllerTest.java
+++ src/test/java/com/websitesmonitorization/controller/WebControllerTest.java
@@ -2,13 +2,25 @@ package com.websitesmonitorization.controller;
 
 import static org.junit.Assert.*;
 
+import java.sql.SQLException;
+
+import com.websitesmonitorization.model.Web;
+import com.websitesmonitorization.dao.WebDAO;
+
 import org.junit.Test;
 
 public class WebControllerTest {
 
 	@Test
-	public void test() {
-		assertTrue(true);
+	public void getWebById_test() throws SQLException {
+
+		WebDAO webDAO = new WebDAO();
+		
+		Web web1 = webDAO.getWebByID(1);
+		Web web2 = new Web();
+		web2.setIdWeb(1);
+				
+		assertEquals(web1, web2);
 	}
 
 }

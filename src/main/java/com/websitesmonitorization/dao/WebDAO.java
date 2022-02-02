b55532273a468fac85b36251ee2a0082998b3345
ijencinas <ij.encinas@alumnos.upm.es>
2022-02-02 12:42:20 +0100

    Refactorizar las pruebas de integracion

--- src/main/java/com/websitesmonitorization/dao/WebDAO.java
+++ src/main/java/com/websitesmonitorization/dao/WebDAO.java
@@ -1,6 +1,8 @@
 package com.websitesmonitorization.dao;
 
+import com.websitesmonitorization.dao.connector.Connector;
 import com.websitesmonitorization.model.Web;
+import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
 import org.springframework.stereotype.Repository;
@@ -14,8 +16,10 @@ public class WebDAO extends GenericDAO{
     public Web getWebByID(int id) throws SQLException {
         String query = "SELECT * FROM Webs WHERE id ="+id;
         Web web = null;
-        try (Connection conn = connector.getConnection()) {
-             web = queryRunner.query(conn, query, new BeanHandler<>(Web.class));
+        Connector conector = new Connector();
+        QueryRunner qr = new QueryRunner();
+        try (Connection conn = conector.getConnection()) {
+             web = qr.query(conn, query, new BeanHandler<>(Web.class));
         }
 
         return web;
@@ -23,8 +27,10 @@ public class WebDAO extends GenericDAO{
 
     public void deleteWebByID(int id) throws SQLException {
         String query = "DELETE FROM webs WHERE id ="+id;
-        try (Connection conn = connector.getConnection()) {
-            int id0=queryRunner.update(conn, query, new ScalarHandler<Integer>());
+        Connector conector = new Connector();
+        QueryRunner qr = new QueryRunner();
+        try (Connection conn = conector.getConnection()) {
+            int id0=qr.update(conn, query, new ScalarHandler<Integer>());
         }
 
         return;
@@ -38,8 +44,10 @@ public class WebDAO extends GenericDAO{
         String query1 = "INSERT INTO webs (url,nombre, tipo, `update`) VALUES (\"https://www.oracle.com/es/index.html\", \"oracle\", \"informatica\", \"2021-11-09 14:04:05\");";
         String query2 = "SELECT LAST_INSERT_ID();";
         int id=-1;
-        try (Connection conn = connector.getConnection()) {
-            id=queryRunner.insert(conn, query1, new ScalarHandler<Integer>());
+        Connector conector = new Connector();
+        QueryRunner qr = new QueryRunner();
+        try (Connection conn = conector.getConnection()) {
+            id=qr.insert(conn, query1, new ScalarHandler<Integer>());
             //id=queryRunner.query(conn, query2, new ScalarHandler<Integer>());
 
         }
@@ -50,8 +58,10 @@ public class WebDAO extends GenericDAO{
     public void updateWebByID(int id, Web web) throws SQLException {
         String query = "UPDATE webs SET url ="+web.getUrl()+", nombre="+web.getNombreWeb()+
                 ", tipo="+web.getTipoWeb() +", update="+web.getUpdate()+" WHERE id ="+id;
-        try (Connection conn = connector.getConnection()) {
-            queryRunner.update(conn, query);
+        Connector conector = new Connector();
+        QueryRunner qr = new QueryRunner();
+        try (Connection conn = conector.getConnection()) {
+            qr.update(conn, query);
         }
 
         return;
